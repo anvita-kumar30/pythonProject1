ca848da53dcb2238a03d104d9613bca30761cbda
Anvita <kumaranvita30@gmail.com>
2024-07-25 00:27:54 +0530

    init

--- Strings.py
+++ Strings.py
@@ -1,8 +1,19 @@
-a = input("Enter your name: ")
-print("My name is", a)
+name = "Harry"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+apple = '''He said, 
+Hi Harry
+hey I am good
+"I want to eat an apple'''
 
-x = input("Enter first number: ")
-y = input("Enter second number: ")
-print(x  + y)
-
-print(int(x) + int(y))
+print("Hello, " + name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
